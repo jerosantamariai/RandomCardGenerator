6364582860a46f3452735cc9b62bfe43010dd78a
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-03-05 23:51:10 -0300

    first advance

---
+++ main.js
@@ -0,0 +1,24 @@
+let crdnum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let colour = ["spade", "cubs", "hearts", "diamonds"];
+
+let rnd1 = Math.floor(Math.random()*crdnum.length);
+let rnd2 = Math.floor(Math.random()*colour.length);
+
+
+document.querySelector(".center").innerHTML = card(crdnum[rnd1]);
+document.querySelectorAll(".top, .bottom").classList.add(colour[rnd2]);
+
+
+
+function card(num) {
+switch(num){
+    case 1: return "A";
+    case 11: return "J";
+    case 12: return "Q";
+    case 13: return "K";
+    default: return num;
+}
+}
+let a = document.createElement("p");
+let b = document.createTextNode("Hello World!");
+a.appendChild(b);
